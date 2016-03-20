3214202aa42714f1953f7b05e942d24778911f12
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-03-20 20:52:39 +0100

    [emacs] Add thrift-mode package

---
+++ Emacs/elpa/thrift-20140312.1348/thrift-autoloads.el
@@ -0,0 +1,23 @@
+;;; thrift-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "thrift" "thrift.el" (22253 50345 124506 716000))
+;;; Generated autoloads from thrift.el
+
+(add-to-list 'auto-mode-alist '("\\.thrift\\'" . thrift-mode))
+
+(autoload 'thrift-mode "thrift" "\
+Mode for editing Thrift files.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; thrift-autoloads.el ends here
