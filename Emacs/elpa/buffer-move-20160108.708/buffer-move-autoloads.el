63c458d873e8ce58f9491fdb62afd5a1d92e5728
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-03-21 18:31:07 +0100

    [emacs] Add buffer-move elpa pacakge

---
+++ Emacs/elpa/buffer-move-20160108.708/buffer-move-autoloads.el
@@ -0,0 +1,53 @@
+;;; buffer-move-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "buffer-move" "buffer-move.el" (22256 6050
+;;;;;;  0 0))
+;;; Generated autoloads from buffer-move.el
+
+(autoload 'buf-move-up "buffer-move" "\
+Swap the current buffer and the buffer above the split.
+   If there is no split, ie now window above the current one, an
+   error is signaled.
+
+\(fn)" t nil)
+
+(autoload 'buf-move-down "buffer-move" "\
+Swap the current buffer and the buffer under the split.
+   If there is no split, ie now window under the current one, an
+   error is signaled.
+
+\(fn)" t nil)
+
+(autoload 'buf-move-left "buffer-move" "\
+Swap the current buffer and the buffer on the left of the split.
+   If there is no split, ie now window on the left of the current
+   one, an error is signaled.
+
+\(fn)" t nil)
+
+(autoload 'buf-move-right "buffer-move" "\
+Swap the current buffer and the buffer on the right of the split.
+   If there is no split, ie now window on the right of the current
+   one, an error is signaled.
+
+\(fn)" t nil)
+
+(autoload 'buf-move "buffer-move" "\
+Begin moving the current buffer to different windows.
+
+Use the arrow keys to move in the desired direction.  Pressing
+any other key exits this function.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; buffer-move-autoloads.el ends here
---
+++ Emacs/elpa/buffer-move-20160108.708/buffer-move-pkg.el
@@ -0,0 +1,1 @@
+(define-package "buffer-move" "20160108.708" "easily swap buffers" 'nil :url "https://github.com/lukhas/buffer-move" :keywords '("lisp" "convenience"))
