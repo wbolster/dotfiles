432402cdc7e94d68a8a4a276b5e1b7e5910cca07
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-05-20 23:14:19 +0200

    [emacs] Upgrade melpa packages

---
+++ Emacs/elpa/js3-mode-20160515.850/js3-mode-autoloads.el
@@ -0,0 +1,43 @@
+;;; js3-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "js3" "js3.el" (22335 30455 240577 101000))
+;;; Generated autoloads from js3.el
+ (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+
+(autoload 'js3-mode "js3" "\
+Major mode for editing JavaScript code.
+
+\\{js3-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "js3-mode" "js3-mode.el" (22335 30455 232577
+;;;;;;  27000))
+;;; Generated autoloads from js3-mode.el
+ (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+
+(autoload 'js3-mode "js3-mode" "\
+Major mode for editing JavaScript code.
+
+\\{js3-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("js3-mode-pkg.el") (22335 30455 251642
+;;;;;;  971000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; js3-mode-autoloads.el ends here
--- Emacs/elpa/yaml-mode-20160425.320/yaml-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yaml-mode" "20160425.320" "Major mode for editing YAML files" '((emacs "24.1")) :keywords '("data" "yaml"))
