117473ecb85869457b4d3dd1325ff115e2435b29
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-08-20 00:39:33 +0200

    [emacs] Upgrade melpa packages

--- Emacs/elpa/git-commit-20160519.950/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20160519.950" "Edit Git commit messages" '((emacs "24.4") (dash "20151021.113") (with-editor "20160408.201")) :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
--- Emacs/elpa/visual-fill-column-20160801.710/visual-fill-column-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "visual-fill-column" "20160801.710" "fill-column for visual-line-mode" '((emacs "24.3")))
