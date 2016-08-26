c898d40164f79cf2bd4004b42a9721b77135f4bb
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-08-26 12:15:10 +0200

    [emacs] Upgrade dumb-jump

--- Emacs/elpa/dumb-jump-20160825.1130/dumb-jump-autoloads.el
+++ Emacs/elpa/dumb-jump-20160825.1130/dumb-jump-autoloads.el
@@ -9,17 +9,28 @@
 (defvar dumb-jump-mode-map (let ((map (make-sparse-keymap))) (define-key map (kbd "C-M-g") 'dumb-jump-go) (define-key map (kbd "C-M-p") 'dumb-jump-back) (define-key map (kbd "C-M-q") 'dumb-jump-quick-look) map))
 
 (autoload 'dumb-jump-back "dumb-jump" "\
-Jump back to where the last jump was done
+Jump back to where the last jump was done.
 
 \(fn)" t nil)
 
 (autoload 'dumb-jump-quick-look "dumb-jump" "\
-Run dump-jump-go in quick look mode. That is, show a tooltip of where it would jump instead
+Run dump-jump-go in quick look mode.  That is, show a tooltip of where it would jump instead.
+
+\(fn)" t nil)
+
+(autoload 'dumb-jump-go-other-window "dumb-jump" "\
+Like 'dumb-jump-go' but use 'find-file-other-window' instead of 'find-file'.
+
+\(fn)" t nil)
+
+(autoload 'dumb-jump-go-current-window "dumb-jump" "\
+Like dumb-jump-go but always use 'find-file'.
 
 \(fn)" t nil)
 
 (autoload 'dumb-jump-go "dumb-jump" "\
-Go to the function/variable declaration for thing at point
+Go to the function/variable declaration for thing at point.
+When USE-TOOLTIP is t a tooltip jump preview will show instead.
 
 \(fn &optional USE-TOOLTIP)" t nil)
 
