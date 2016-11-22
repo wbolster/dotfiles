23e04a0dff9eff47b398067b12861841ae869096
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-11-22 21:48:38 +0100

    [emacs] add my own evil-text-object-python package
    …from melpa instead of loading it via init-local.el.

---
+++ Emacs/elpa/evil-text-object-python-20160815.141/evil-text-object-python-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-text-object-python" "20160815.141" "Python specific evil text objects" '((emacs "24") (evil "1.2.12")) :url "https://github.com/wbolster/evil-text-object-python" :keywords '("evil" "python" "text-object"))
