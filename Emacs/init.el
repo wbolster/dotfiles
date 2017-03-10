0c2e4f34d00283b21924594c8af588058b14ba15
Wouter Bolsterlee <wouter@bolsterl.ee>
2017-03-11 00:50:07 +0100

    [emacs] remove unused "after" macro
    with-eval-after-load seems to do the same anyway.

--- Emacs/init.el
+++ Emacs/init.el
@@ -19,14 +19,9 @@
 
 (require 'package)
 (package-initialize)
+
 (require 'auto-compile)
 (auto-compile-on-load-mode)
-
-(defmacro after (feature &rest body)
-  "After FEATURE is loaded, evaluate BODY."
-  (declare (indent defun))
-  `(eval-after-load ,feature
-     '(progn ,@body)))
 
 
 ;;;
