cbed70248fb7a6ff13af6201ab6dcf84cca1e0f7
wouter bolsterlee <wouter@bolsterl.ee>
2017-07-18 17:16:55 +0200

    [emacs] make magit use git cherry-pick --ff -x by default

--- Emacs/init.el
+++ Emacs/init.el
@@ -1758,6 +1758,7 @@ defined as lowercase."
    magit-branch-prefer-remote-upstream '("master")
    magit-branch-read-upstream-first nil
    magit-completing-read-function 'ivy-completing-read
+   magit-cherry-pick-arguments '("-x" "--ff")
    magit-merge-arguments '("--no-ff")
    magit-popup-show-help-echo nil
    magit-prefer-remote-upstream t
