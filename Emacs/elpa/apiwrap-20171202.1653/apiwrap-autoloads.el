0de1ebcb2dcb23332963871fa0d0b574a25e8dd7
wouter bolsterlee <wouter@bolsterl.ee>
2017-12-20 17:41:28 +0100

    [emacs] add back magithub package

---
+++ Emacs/elpa/apiwrap-20171202.1653/apiwrap-autoloads.el
@@ -0,0 +1,15 @@
+;;; apiwrap-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("apiwrap.el") (23098 35756 673856 599000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; apiwrap-autoloads.el ends here
---
+++ Emacs/elpa/magithub-20171214.737/magithub-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magithub" "20171214.737" "Magit interfaces for Github"
+  '((emacs "25")
+    (magit "2.8")
+    (s "1.12.0")
+    (ghub+ "0.2")
+    (git-commit "2.8")
+    (markdown-mode "2.3"))
+  :url "https://github.com/vermiculus/magithub" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
