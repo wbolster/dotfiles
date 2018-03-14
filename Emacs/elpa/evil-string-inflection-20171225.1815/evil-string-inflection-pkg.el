6c8e92a13ef68d0816178e35329a0befd778370e
wouter bolsterlee <wouter@bolsterl.ee>
2018-03-14 22:25:56 +0100

    [emacs] upgrade evil-string-inflection

--- Emacs/elpa/evil-string-inflection-20171225.1815/evil-string-inflection-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-string-inflection" "20171225.1815" "snake_case -> CamelCase -> etc. for text objects" '((emacs "24") (evil "1.2.13") (string-inflection "1.0.6")) :commit "ac261bee68444c2cb9aaab25b58509e8f58efe35" :url "https://github.com/ninrod/evil-string-inflection")
--- Emacs/elpa/evil-string-inflection-20180313.1055/evil-string-inflection-autoloads.el
+++ Emacs/elpa/evil-string-inflection-20180313.1055/evil-string-inflection-autoloads.el
@@ -4,10 +4,11 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "evil-string-inflection" "evil-string-inflection.el"
-;;;;;;  (23106 39697 650395 625000))
+;;;;;;  (23209 37669 453212 317000))
 ;;; Generated autoloads from evil-string-inflection.el
 
-(evil-define-operator evil-operator-string-inflection (beg end _type) "Define a new evil operator that cicles underscore -> UPCASE -> CamelCase." :move-point nil (interactive "<R>") (let ((str (buffer-substring-no-properties beg end))) (save-excursion (delete-region beg end) (insert (string-inflection-all-cycle-function str)))))
+(autoload 'evil-string-inflection "evil-string-inflection.el" "\
+Define a new evil operator that cicles underscore -> UPCASE -> CamelCase." t)
 
 ;;;***
 
