a24055d471d1e3af1407f9c9169360e82e1782f1
wouter bolsterlee <wouter@bolsterl.ee>
2019-03-28 18:15:02 +0100

    [emacs] upgrade melpa packages

--- Emacs/elpa/dumb-jump-20190315.532/dumb-jump-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dumb-jump" "20190315.532" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.20.0") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :commit "59f91b408f6228be035ece10a79b5d460430116a" :keywords '("programming") :authors '(("jack angers")) :maintainer '("jack angers"))
---
+++ Emacs/elpa/git-commit-20190319.2352/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190319.2352" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "7889f5d8803f8345a5715e0023470ab33da62e45" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
