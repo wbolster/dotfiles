836be8732faab1f13d91c9c84ac71bdd1827ded1
wouter bolsterlee <wouter@bolsterl.ee>
2019-05-03 20:05:05 +0200

    [emacs] upgrade packages

--- Emacs/elpa/git-commit-20190426.1503/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190426.1503" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "88222678bba51b27bd27d967f6c253baa35cf5ef" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- Emacs/elpa/jq-format-20190426.2131/jq-format-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "jq-format" "20190426.2131" "Reformat JSON and JSONLines using jq" '((emacs "24") (reformatter "0.3")) :authors '(("wouter bolsterlee" . "wouter@bolsterl.ee")) :maintainer '("wouter bolsterlee" . "wouter@bolsterl.ee") :url "https://github.com/wbolster/emacs-jq-format")
