31e4daf989d00f01319572050bc9182e1629eec1
wouter bolsterlee <wouter@bolsterl.ee>
2020-10-01 12:04:36 +0200

    [emacs] show git log since/until flags in dispatch menu

--- Emacs/etc/transient/levels.el
+++ Emacs/etc/transient/levels.el
@@ -1,4 +1,6 @@
 ((magit-log
+  (transient:magit-log:--until= . 2)
+  (transient:magit-log:--since= . 2)
   (magit-log-merged . 1)
   (transient:magit-log:--first-parent . 1)
   (transient:magit-log:--no-merges . 1)))
