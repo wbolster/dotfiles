0c2314c251aada0b3fc00973765d2399cedeba3c
wouter bolsterlee <wouter@bolsterl.ee>
2024-07-22 11:11:53 +0200

    emacs: upgrade magit and some related packages

---
+++ emacs/packages/magit-20240721.1509/magit-pkg.el
@@ -0,0 +1,20 @@
+(define-package "magit" "20240721.1509" "A Git porcelain inside Emacs."
+  '((emacs "26.1")
+    (compat "30.0.0.0")
+    (dash "20240510")
+    (git-commit "20240623")
+    (magit-section "20240710")
+    (seq "2.24")
+    (transient "20240629")
+    (with-editor "20240702"))
+  :commit "f31cf79b2731765d63899ef16bc8be0fa2cc7d32" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
