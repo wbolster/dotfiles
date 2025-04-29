3e63a4404f578bf02b370555cc4aa8db585de434
wouter bolsterlee <wouter@bolsterl.ee>
2025-04-29 10:37:11 +0200

    emacs: add lsp-origami package

---
+++ emacs/packages/lsp-origami-20230815.704/lsp-origami-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-origami" "20230815.704"
+  "Origami.el support for lsp-mode."
+  '((emacs    "27.1")
+    (origami  "1.0")
+    (lsp-mode "6.1"))
+  :url "https://github.com/emacs-lsp/lsp-origami"
+  :commit "86aa06517910141c3d5054eea5f7723461fce6a6"
+  :revdesc "86aa06517910"
+  :keywords '("languages" "lsp-mode"))
