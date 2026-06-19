6ccc7c1aeea6a974c4aae651d7e078c9cda13ed4
wouter bolsterlee <wouter@bolsterl.ee>
2026-06-19 23:53:56 +0200

    emacs: corfu: install package

---
+++ emacs/packages/corfu-20260519.1053/corfu-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "corfu" "20260519.1053"
+  "COmpletion in Region FUnction."
+  '((emacs  "29.1")
+    (compat "31"))
+  :url "https://github.com/minad/corfu"
+  :commit "4a9c67da16eb64cadaa4bfcc16713188145c83da"
+  :revdesc "4a9c67da16eb"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
