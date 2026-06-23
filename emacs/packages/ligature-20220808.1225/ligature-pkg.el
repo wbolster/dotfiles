b43b30b5f6c3c3a8f2b97c44d616ce0bdfb14fff
wouter bolsterlee <wouter@bolsterl.ee>
2026-06-23 18:47:13 +0200

    emacs: ligature: add package

---
+++ emacs/packages/ligature-20220808.1225/ligature-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ligature" "20220808.1225"
+  "Display typographical ligatures in major modes."
+  '((emacs "28"))
+  :url "https://www.github.com/mickeynp/ligature.el"
+  :commit "89cbd67a815f61e5001f19d64d6ec1771e867742"
+  :revdesc "89cbd67a815f"
+  :keywords '("tools" "faces")
+  :authors '(("Mickey Petersen" . "mickey@masteringemacs.org"))
+  :maintainers '(("Mickey Petersen" . "mickey@masteringemacs.org")))
