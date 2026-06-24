0100fd87bbb8ae39e72a172227a3eba406991832
wouter bolsterlee <wouter@bolsterl.ee>
2026-06-24 23:55:30 +0200

    emacs: origami: tidy

--- emacs/init.el
+++ emacs/init.el
@@ -1596,6 +1596,67 @@
   :custom
   (org-ellipsis " […]"))
 
+(use-package origami
+  :defer t
+  :custom-face
+  (origami-fold-replacement-face ((t (:inherit magit-diff-context-highlight))))
+  (origami-fold-fringe-face ((t (:inherit magit-diff-context-highlight))))
+  :commands
+  w/origami-mode-toggle
+  :functions
+  origami-close-all-nodes
+  origami-show-only-node
+  w/origami-parser-imenu-flat
+  :custom
+  (origami-show-fold-header t)
+  :config
+  (face-spec-reset-face 'origami-fold-header-face)
+
+  (defun w/origami-mode-toggle ()
+    (interactive)
+    (origami-mode 'toggle)
+    (when origami-mode
+      (if (> (point) 1)
+          (origami-show-only-node (current-buffer) (point))
+        (origami-close-all-nodes (current-buffer)))))
+
+  (defun w/origami-parser-imenu-flat (create)
+    "Origami parser producing folds for each imenu entry, without nesting."
+    (lambda (content)
+      (let ((orig-major-mode major-mode))
+        (with-temp-buffer
+          (insert content)
+          (funcall orig-major-mode)
+          (let* ((items
+                  (-as-> (imenu--make-index-alist t) items
+                         (-flatten items)
+                         (-filter 'listp items)))
+                 (positions
+                  (-as-> (-map #'cdr items) positions
+                         (-filter 'identity positions)
+                         (-map-when 'markerp 'marker-position positions)
+                         (-filter 'natnump positions)
+                         (cons (point-min) positions)
+                         (-snoc positions (point-max))
+                         (-sort '< positions)
+                         (-uniq positions)))
+                 (ranges
+                  (-zip-pair positions (-map '1- (cdr positions))))
+                 (fold-nodes
+                  (--map
+                   (-let*
+                       (((range-beg . range-end) it)
+                        (line-beg
+                         (progn (goto-char range-beg)
+                                (line-beginning-position)))
+                        (offset
+                         (- (min (line-end-position) range-end) line-beg))
+                        (fold-node
+                         (funcall create line-beg range-end offset nil)))
+                     fold-node)
+                   ranges)))
+            fold-nodes))))))
+
 (use-package outline
   :defer t
   :delight
@@ -3060,66 +3121,6 @@ defined as lowercase."
   (interactive)
   (setopt fill-column most-positive-fixnum)
   (auto-fill-mode -1))
-
-(use-package origami
-  :custom
-  (origami-show-fold-header t)
-
-  :custom-face
-  (origami-fold-replacement-face ((t (:inherit magit-diff-context-highlight))))
-  (origami-fold-fringe-face ((t (:inherit magit-diff-context-highlight))))
-
-  :commands
-  w/origami-parser-imenu-flat
-  w/origami-mode-toggle
-
-  :config
-  (face-spec-reset-face 'origami-fold-header-face)
-
-  (defun w/origami-mode-toggle ()
-    (interactive)
-    (origami-mode 'toggle)
-    (when origami-mode
-      (if (> (point) 1)
-          (origami-show-only-node (current-buffer) (point))
-        (origami-close-all-nodes (current-buffer)))))
-
-  (defun w/origami-parser-imenu-flat (create)
-    "Origami parser producing folds for each imenu entry, without nesting."
-    (lambda (content)
-      (let ((orig-major-mode major-mode))
-        (with-temp-buffer
-          (insert content)
-          (funcall orig-major-mode)
-          (let* ((items
-                  (-as-> (imenu--make-index-alist t) items
-                         (-flatten items)
-                         (-filter 'listp items)))
-                 (positions
-                  (-as-> (-map #'cdr items) positions
-                         (-filter 'identity positions)
-                         (-map-when 'markerp 'marker-position positions)
-                         (-filter 'natnump positions)
-                         (cons (point-min) positions)
-                         (-snoc positions (point-max))
-                         (-sort '< positions)
-                         (-uniq positions)))
-                 (ranges
-                  (-zip-pair positions (-map '1- (cdr positions))))
-                 (fold-nodes
-                  (--map
-                   (-let*
-                       (((range-beg . range-end) it)
-                        (line-beg
-                         (progn (goto-char range-beg)
-                                (line-beginning-position)))
-                        (offset
-                         (- (min (line-end-position) range-end) line-beg))
-                        (fold-node
-                         (funcall create line-beg range-end offset nil)))
-                     fold-node)
-                   ranges)))
-            fold-nodes))))))
 
 ;; todo https://github.com/sshaw/copy-as-format/issues/2
 (use-package copy-as-format
