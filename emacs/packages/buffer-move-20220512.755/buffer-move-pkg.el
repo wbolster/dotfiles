ccbca86ab74648e780dbd585c67ebd819f48c190
wouter bolsterlee <wouter@bolsterl.ee>
2026-06-26 12:23:56 +0200

    emacs: buffer-move: drop package

--- emacs/packages/buffer-move-20220512.755/buffer-move-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "buffer-move" "20220512.755"
-  "Easily swap buffers."
-  '((emacs "24.1"))
-  :url "https://github.com/lukhas/buffer-move/"
-  :commit "e7800b3ab1bd76ee475ef35507ec51ecd5a3f065"
-  :revdesc "e7800b3ab1bd"
-  :keywords '("convenience"))
