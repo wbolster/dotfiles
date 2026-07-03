a3e2d9a78ec944774c3ad173ec0c0ae4bb86109f
wouter bolsterlee <wouter@bolsterl.ee>
2026-07-03 23:58:56 +0200

    emacs: wgrep: add package

---
+++ emacs/packages/wgrep-20230203.1214/wgrep-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "wgrep" "20230203.1214"
+  "Writable grep buffer."
+  '((emacs "25.1"))
+  :url "https://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep.el"
+  :commit "b4d69280d8a6a5ded1597e02afbaa811a160383b"
+  :revdesc "b4d69280d8a6"
+  :keywords '("grep" "edit" "extensions")
+  :authors '(("Masahiro Hayashi" . "mhayashi1120@gmail.com"))
+  :maintainers '(("Masahiro Hayashi" . "mhayashi1120@gmail.com")))
