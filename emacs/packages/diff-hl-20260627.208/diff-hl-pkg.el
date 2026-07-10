1d6bb75ecf17e642a476cc57309b4bc04dae32ce
wouter bolsterlee <wouter@bolsterl.ee>
2026-07-10 11:00:06 +0200

    emacs: diff-hl: add package

---
+++ emacs/packages/diff-hl-20260627.208/diff-hl-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "diff-hl" "20260627.208"
+  "Highlight uncommitted changes using VC."
+  '((cl-lib "0.2")
+    (emacs  "27.1"))
+  :url "https://github.com/dgutov/diff-hl"
+  :commit "2d7d0714d9637f54af672987c65b6973b31e56a2"
+  :revdesc "2d7d0714d963"
+  :keywords '("vc" "diff")
+  :authors '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainers '(("Dmitry Gutov" . "dmitry@gutov.dev")))
