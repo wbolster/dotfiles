2389c0a3f48da15ba2068cd119d07b9aba3ae7e7
wouter bolsterlee <wouter@bolsterl.ee>
2026-07-10 11:00:06 +0200

    emacs: embark, embark-consult: add packages

---
+++ emacs/packages/embark-20260610.302/embark-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "embark" "20260610.302"
+  "Conveniently act on minibuffer completions."
+  '((emacs  "29.1")
+    (compat "31"))
+  :url "https://github.com/oantolin/embark"
+  :commit "350ca86924c5027e80875943fba7b912a71e5791"
+  :revdesc "350ca86924c5"
+  :keywords '("convenience")
+  :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainers '(("Omar Antolín Camarena" . "omar@matem.unam.mx")))
---
+++ emacs/packages/embark-consult-20260503.118/embark-consult-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "embark-consult" "20260503.118"
+  "Consult integration for Embark."
+  '((emacs   "29.1")
+    (compat  "31")
+    (embark  "1.1")
+    (consult "3.2"))
+  :url "https://github.com/oantolin/embark"
+  :commit "ec5dd1475595277ef908567d0a18d32f1c40bc91"
+  :revdesc "ec5dd1475595"
+  :keywords '("convenience")
+  :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainers '(("Omar Antolín Camarena" . "omar@matem.unam.mx")))
